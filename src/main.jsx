587d246b074247ce7b01b4f2f5fdf7606cb4c9b0
Ersan Karimi <73420137+ersankarimi@users.noreply.github.com>
2022-06-22 15:25:10 +0800

    Update main.jsx
    - Membungkus App komponen dengan SidebarContextProvider komponen yang mana itu sudah dibungkus dengan SidebarContext

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { SidebarContextProvider } from './context'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<BrowserRouter>
 		<React.StrictMode>
-			<App />
+			<SidebarContextProvider>
+				<App />
+			</SidebarContextProvider>
 		</React.StrictMode>
 	</BrowserRouter>
 )
